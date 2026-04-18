cd1d67f6a1d9457e6fa0fe6c309ea929eb87ec86
Sofiabb23 <sofia.brandan@davinci.edu.ar>
2026-04-17 23:47:48 -0300

    Clase-1

---
+++ Semana-01/Persona.js
@@ -0,0 +1,17 @@
+class Persona {
+    // Atributos
+    nombre = '';
+    apellido = '';
+    // Constructor
+    constructor(nombre, apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        console.log ('Hola desde el constructor');
+    }
+    // Métodos
+    saludar() {
+        console.log(`Hola, mi nombre es ${this.nombre}`);
+    }
+}
+
+export default Persona
---
+++ Semana-01/index.js
@@ -0,0 +1,8 @@
+import Persona from './Persona.js';
+
+const nombre = 'Sofia';
+console.log(`Hola ${nombre}`);
+
+
+    const p1 = new Persona('Sofia', 'Gomez');
+    p1.saludar();
