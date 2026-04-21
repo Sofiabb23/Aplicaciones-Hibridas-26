4c5d9e1bcf61ba701b94852608b0c72ee6898936
Sofiabb23 <sofia.brandan@davinci.edu.ar>
2026-04-21 05:35:43 -0300

    modificacion

--- Semana-03/index.js
+++ Semana-03/index.js
@@ -1,28 +1,43 @@
+import express from 'express';
 import User from "./models/User.js";
+import chalk from "chalk";
+//console.log( chalk.green('Node 35') );
+const PORT = 3000
 
-console.log('Inicio');
 const userModel = new User();
 
-const user1 = {
-    name: "Jose",
-    email: "jose@davinci.edu.ar"
-}
+const app = express();
 
-const user2 = {
-    name: "Sofia",
-    email: "sofia@davinci.edu.ar"
-}
-
-//userModel.save( user1);
-//userModel.save( user2);
-
-
-console.log('Fin');
-
-userModel.find().then( users => {
-console.table(users);
+app.get('/', (request, response) => {
+    response.send('<h1> API REST hola desde el cambio </h1>');
 })
 
-userModel.findById('35f3167b-5cb7-4eea-80dd-433600d5448c').then( user => {
-    console.log({user});
+app.get('/api/users', async (req,res) => {
+    try{
+        const users = await userModel.find();
+        console.log(users);
+        res.json({ status: 'Ok', data: users});
+    } catch (error) {
+        res.status(500)({ status: 'Error', data: []});
+        console.error(error);
+    }
+    
+})
+
+app.post('/api/users', (req,res) => {
+    try{
+        console.log('POST');
+        res.json({ status: 'Ok', data: []});
+
+    } catch (error) {
+        res.status(500)({ status: 'Error', data: []});
+        console.error(error);
+    }
+
+    
+})
+
+
+app.listen( PORT, () => {
+    console.log( chalk.green(`Servidor Web en el puerto ${PORT}`));
 })
