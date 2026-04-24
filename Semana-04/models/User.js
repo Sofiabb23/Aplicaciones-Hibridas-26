69cc510e4ab6c0539ed30a0c548b6d37344e5e60
Sofiabb23 <sofia.brandan@davinci.edu.ar>
2026-04-23 21:48:02 -0300

    Agrego Semana-04

---
+++ Semana-04/models/User.js
@@ -0,0 +1,48 @@
+import fs from 'fs/promises'
+
+//clase usuarios
+class User {
+    path = './data/users.json';
+    list = []
+    constructor(){
+
+    }
+
+    async readJson(){
+        const data = await fs.readFile(this.path)
+        const json = JSON.parse(data);
+        this.users = json;
+
+    }
+    
+    async save(user){
+        const id = crypto.randomUUID();
+        user.id = id;
+        this.list.push(user);
+        const data = JSON.stringify(this.list, null, 2);
+        await fs.writeFile(this.path, data);
+        console.log('Usuario Guardado');
+    }
+
+    async find(){
+        await this.readJson()
+        return this.users
+    }
+
+    async findById(id){
+        await this.readJson();
+        const user = this.users.find( u => u.id === id);
+        return user;
+    }
+
+
+    // async deleteById( id ){
+    //     await this.readJson();
+    // }
+    // async updateById( id, user){
+
+    // }
+
+}
+
+export default User
