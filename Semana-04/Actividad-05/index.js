abbcdd042ca9550ee70f18e36a8a4ef708c710e3
Sofiabb23 <sofia.brandan@davinci.edu.ar>
2026-04-24 01:50:37 -0300

    Agrego modificaciones

--- Semana-04/Actividad-05/index.js
+++ Semana-04/Actividad-05/index.js
@@ -1,68 +1,126 @@
-import express, { json } from 'express';
-import ProductManager from "./ProductManager.js";
+import express from "express";
+import ProductManager from "./models/ProductManager.js";
 import chalk from "chalk";
 
 const PORT = 4000;
 
-const productManager = new ProductManager("./products.json");
-
 const app = express();
 app.use(express.json());
 
+const productManager = new ProductManager("./data/products.json");
 
-app.get('/', (req, res) => {
-    res.send('<h1> API REST - Productos </h1>');
-});
+    app.get("/", (req, res) => {
+        res.send("<h1>API REST - Productos Tecnologicos</h1>");
+    });
 
+//Obtener productos
+    app.get("/api/products", async (req, res) => {
+        try {
+            const products = await productManager.getProducts();
 
-app.get('/api/products', async (req, res) => {
-    try {
-        const products = await productManager.getProducts();
-        console.log(products);
-
-        res.json({
-            status: 'Ok',
-            data: products
-        });
-
-    } catch (error) {
-        res.status(500).json({
-            status: 'Error',
-            data: []
-        });
-        console.error(error);
-    }
-});
-
-app.get('/api/products/:id', async (req, res) => {
-    try {
-        const id = parseInt(req.params.id);
-
-        const product = await productManager.getProductById(id);
-
-        if (!product) {
-            return res.status(404).json({
-                status: 'error',
-                data: "Producto no encontrado"
+            res.json({
+                status: "Ok", data: products
             });
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ status: "error", data: [] });
         }
+    });
 
-        res.json({
-            status: 'Ok',
-            data: product
+// Obtener producto por ID
+    app.get("/api/products/:id", async (req, res) => {
+        try {
+            const id = parseInt(req.params.id);
+            const product = await productManager.getProductById(id);
+            if (!product) {
+            return res.status(404).json({
+                status: "error", data: "Product not found"
+            });
+            }
+            res.json({ 
+                status: "ok", data: product
+            });
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ status: "error", data: null });
+        }
+    });
+
+// Agregar Producto
+    app.post("/api/products", async (req, res) => {
+        try {
+            const { name, description, price, stock } = req.body;
+
+            if (!name || !description || price === undefined || stock === undefined) {
+                return res.status(400).json({
+                    status: "error", data: "Todos los campos son obligatorios"
+                });
+            }
+
+        await productManager.addProduct({ name, description, price, stock });
+
+        res.status(201).json({ 
+            status: "ok", data: "Producto Agregado"
         });
 
-    } catch (error) {
-        res.status(500).json({
-            status: 'error',
-            data: null
-        });
-        console.error(error);
-    }
-});
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ status: "error", data: null });
+        }
+    });
+
+// Actualizar
+    app.put("/api/products/:id", async (req, res) => {
+        try {
+            const id = parseInt(req.params.id);
+            const updatedData = req.body;
+
+            const updated = await productManager.updateProductById(id, updatedData);
+
+            if (!updated || Object.keys(updated).length === 0) {
+                return res.status(404).json({
+                    status: "error", data: "Product not found"
+                });
+            }
+
+            res.json({
+                status: "ok", data: updated
+            });
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ status: "error", data: null });
+        }
+    });
+
+// Eliminar producto
+    app.delete("/api/products/:id", async (req, res) => {
+        try {
+            const id = parseInt(req.params.id);
+
+            const product = await productManager.getProductById(id);
+
+            if (!product) {
+                return res.status(404).json({
+                    status: "error", data: "Product not found"
+                });
+            }
+
+            await productManager.deleteProductById(id);
+
+            res.json({
+                status: "ok", data: "Producto Eliminado"
+            });
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ status: "error", data: null });
+        }
+    });
 
 
-// servidor
 app.listen(PORT, () => {
     console.log(chalk.green(`Servidor Web en el puerto ${PORT}`));
 });
